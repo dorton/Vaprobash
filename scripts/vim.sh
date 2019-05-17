89650438caeaebd7b81f181773f5fb8a69f7ac6c
Brian Dorton <brian.dorton@ibm.com>
2019-05-17 11:24:04 -0500

    correct folders this time

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -3,14 +3,10 @@
 echo ">>> Setting up Vim"
 
 
-# Create directories needed for some .vimrc settings
-mkdir -p /home/vagrant/.vim/backup
-mkdir -p /home/vagrant/.vim/swap
-
 sudo apt-get install vim
 
-mkdir /home/.vim_runtime
+mkdir -p /home/vagrant/.vim_runtime
 
-git clone --depth=1 https://github.com/dorton/vimrc.git /home/.vim_runtime
+git clone --depth=1 https://github.com/dorton/vimrc.git /home/vagrant/.vim_runtime
 sudo chown -R vagrant:vagrant /home/vagrant/.vim_runtime
-sudo sh ~/.vim_runtime/install_awesome_vimrc.sh
+sudo sh /home/vagrant/.vim_runtime/install_awesome_vimrc.sh
