33a68112e5d73df0d00dd69fc387b024ec0b1718
Brian Dorton <brian.dorton@ibm.com>
2019-05-17 11:36:17 -0500

    install quiet, assume yes

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install zsh
+sudo apt-get install -qq zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
