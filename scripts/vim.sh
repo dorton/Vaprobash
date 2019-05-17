f7b013882d30c2660cef2b4e39bcc56e7afe7c79
Brian Dorton <brian.dorton@ibm.com>
2019-05-17 11:20:39 -0500

    fix vim folders

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -9,8 +9,8 @@ mkdir -p /home/vagrant/.vim/swap
 
 sudo apt-get install vim
 
-mkdir ~/.vim_runtime
+mkdir /home/.vim_runtime
 
-git clone --depth=1 https://github.com/dorton/vimrc.git ~/.vim_runtime
+git clone --depth=1 https://github.com/dorton/vimrc.git /home/.vim_runtime
 sudo chown -R vagrant:vagrant /home/vagrant/.vim_runtime
 sudo sh ~/.vim_runtime/install_awesome_vimrc.sh
